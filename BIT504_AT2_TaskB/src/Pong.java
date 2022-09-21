0942805f767c93e3c32c7f69030d8c8a94cd73a4
simon357 <simonclarknz@gmail.com>
2022-09-22 00:11:16 +1200

    Experiment 1 New Code replaced

--- BIT504_AT2_TaskB/src/Pong.java
+++ BIT504_AT2_TaskB/src/Pong.java
@@ -1,10 +1,22 @@
+//Experiment 1 - Testing future proofing variables
 
-public class Pong {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		Small change before upload to main branch
-
-	}
-
-}
+import javax.swing.JFrame;
+   
+   public class Pong extends JFrame {
+       
+       private final static String WINDOW_TITLE = "Pong";
+       private final static int WINDOW_WIDTH = 800;
+       private final static int WINDOW_HEIGHT = 600;
+       
+       public Pong() {
+              setTitle(WINDOW_TITLE);
+          setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+              setResizable(false);
+              setVisible(true);
+              setDefaultCloseOperation(EXIT_ON_CLOSE);
+      }
+  
+      public static void main(String[] args) {
+          new Pong();
+      }
+ } 
